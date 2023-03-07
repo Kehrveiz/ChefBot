ba4b50033babc4f44b83f0629ef6c62ca732c1e1
Daniel Ángel <68783745+danielangelarro@users.noreply.github.com>
2023-03-06 22:11:56 -0500

    Add Tokenize layers

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -3,8 +3,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
-import json
 import spacy as sp
+import json
+
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.stem.porter import *
+from ntlk.corpus import stopwords
+
+stop = stopwords.words('spanish')
+stemmer = PorterStemmer()
 
 nlp = sp.load("es_core_news_md")
 
@@ -17,21 +28,20 @@ with open('assest/Data/intents.json') as json_data:
     intents = json.load(json_data)
 
 
-df_cook_others["label"] = df_cook_others.label.map({ 'cook': 1, 'others': 0})
+# Tokenize 'df_cook'
+df_cook['text'] = df_cook['text'].apply(lambda x: nltk.word_tokenize(x), axis = 1)
+df_cook['text'] = df_cook['text'].apply(lambda x: [item for item in x if item not in stop])
+df_cook['text'] = df_cook['text'].apply(lambda x: [stemmer.stem(item) for item in x])
+df_cook['text'] = df_cook['text'].apply(lambda x: ' '.join(x))
+
+# Tokenize 'df_cook_others'
+df_cook_others['text'] = df_cook_others['text'].apply(lambda x: nltk.word_tokenize(x), axis = 1)
+df_cook_others['text'] = df_cook_others['text'].apply(lambda x: [item for item in x if item not in stop])
+df_cook_others['text'] = df_cook_others['text'].apply(lambda x: [stemmer.stem(item) for item in x])
+df_cook_others['text'] = df_cook_others['text'].apply(lambda x: ' '.join(x))
+
 df_cook["label"] = df_cook.label.map({ 'title': 2, 'instructions': 1, 'title_instructions': 0})
-
-X_train, X_test, y_train, y_test = train_test_split(df_cook_varios['text'], df_cook_varios['label'], random_state=0.2)
-
-# Instantiate the CountVectorizer method
-count_vector = CountVectorizer()
-# Fit the training data and then return the matrix
-training_data = count_vector.fit_transform(X_train)
-# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
-testing_data = count_vector.transform(X_test)
-
-naive_bayes = MultinomialNB()
-naive_bayes.fit(training_data, y_train)
-predictions = naive_bayes.predict(testing_data)
+df_cook_others["label"] = df_cook_others.label.map({ 'cook': 1, 'others': 0})
 
 
 def cook_others(text):
@@ -108,6 +118,14 @@ def response_others(tag):
 
 
 def get_response(text):
+
+    # TOKENIZE INPUT
+    # Tokenize 'df_cook'
+    text = text.apply(lambda x: nltk.word_tokenize(x), axis = 1)
+    text = text.apply(lambda x: [item for item in x if item not in stop])
+    text = text.apply(lambda x: [stemmer.stem(item) for item in x])
+    text = text.apply(lambda x: ' '.join(x))
+
     firstIntent = cook_others(text)[0]
     if firstIntent == 0:
         intentOthers = others(text)[0]
